17446c747f71e846a78260e9cb12b20f2055ec04
ryota.suzuki <getty2017@gmail.com>
2018-08-13 16:33:43 +0900

    Have Ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,17 @@
 class RankingsController < ApplicationController
   def want
     @ranking_counts = Want.ranking
+    set_items
+  end
+
+  def have
+    @ranking_counts = Have.ranking
+    set_items
+  end
+
+  private
+
+  def set_items
     @items = Item.find(@ranking_counts.keys)
   end
 end
